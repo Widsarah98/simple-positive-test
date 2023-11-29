10b4a8945d2f65b50f68b89ce2e3215104e4674e
Widsarah98 <sarah.widmer1544@stu.mtec.edu>
2023-11-29 11:36:15 -0700

    I deleted the old files and created new files with new names

---
+++ test/pageobjects/swag.login.page.js
@@ -0,0 +1,32 @@
+import { $ } from '@wdio/globals'
+import Page from './page.js';
+
+class LoginPage extends Page {
+    /**
+     * define selectors using getter methods
+     */
+    get insertUsername () {
+        return $('#user-name');
+    }
+
+    get insertPassword () {
+        return $('#password');
+    }
+
+    get btnLogin () {
+        return $('#login-button');
+    }
+
+   
+    async login (username, password) {
+        await this.insertUsername.setValue(username);
+        await this.insertPassword.setValue(password);
+        await this.btnLogin.click();
+    }
+
+    open () {
+        return super.open('');
+    }
+}
+
+export default new LoginPage();
---
+++ test/pageobjects/swag.secure.page.js
@@ -0,0 +1,12 @@
+import { $ } from '@wdio/globals'
+import Page from './page.js';
+
+
+class SecurePage extends Page {
+    
+    get flashAlert () {
+        return $('.app_logo');
+    }
+}
+
+export default new SecurePage();
